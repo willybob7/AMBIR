7934dc1bd5391508ed6e27fdd4d2b3ba3423bd96
willybob7 <34104463+willybob7@users.noreply.github.com>
2018-09-21 12:46:17 -0400

    Add files via upload

--- Marching_Brass_Code.js
+++ Marching_Brass_Code.js
@@ -6,6 +6,12 @@ const test1 = {
 ser: 904730,
 model: 1120,
 brand: "King",
+description: ["This is a King model 1120 Marching mellophone in ready to play condition. ",
+"The serial number is 904730. It has been ultrasonically cleaned and serviced ",
+"for this auction by a professional repair technician. All slides and valves ",
+"work freely and easily. There are dings, scratches and wear from normal usage. ",
+"There is a case and a Holton 7C mouthpiece."],
+shipping: "$35",
 }
 
 function addPics(pics){
@@ -14,8 +20,8 @@ function addPics(pics){
     let pic = document.createElement("img");
     pic.setAttribute("src", pics[i]);
     pic.setAttribute("alt", "stuff");
-    pic.setAttribute("width", "304");
-    pic.setAttribute("height", "228");
+    // pic.setAttribute("width", "304");
+    pic.setAttribute("height", "80");
     picArea.appendChild(pic);
   }
 }
